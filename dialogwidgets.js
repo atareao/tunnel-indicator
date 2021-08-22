1efebf420d230c8c7fc1319365dd240df9959232
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2021-08-22 07:54:16 +0200

    GNOME 40

--- dialogwidgets.js
+++ dialogwidgets.js
@@ -7,33 +7,63 @@ const Extension = ExtensionUtils.getCurrentExtension();
 const Gettext = imports.gettext.domain(Extension.uuid);
 const _ = Gettext.gettext;
 
-var EntryDialog = GObject.registerClass(
+var KeyValueDialog = GObject.registerClass(
     class EntryDialog extends Gtk.Dialog{
-        _init(label){
-            super._init();
-            let grid = new Gtk.Grid();
-            grid.set_row_spacing(5);
-            grid.set_column_spacing(5);
-            grid.set_margin_start(5);
-            grid.set_margin_end(5);
-            grid.set_margin_top(5);
-            grid.set_margin_bottom(5);
-            this.get_content_area().add(grid);
-            this._label = Gtk.Label.new(label);
-            grid.attach(this._label, 0, 0, 1, 1);
-            this._entry = new Gtk.Entry();
-            grid.attach(this._entry, 1, 0, 1, 1);
+        _init(parent, title, keyLabel, valueLabel, key="", value=""){
+            super._init({
+                title: title,
+                transientFor: parent.get_root(),
+                modal: true,
+                use_header_bar: true
+            });
+            let grid = new Gtk.Grid({
+                marginTop: 10,
+                marginBottom: 10,
+                marginStart: 10,
+                marginEnd:10,
+                columnSpacing: 20,
+                rowSpacing: 20,
+                hexpand: true,
+                vexpand: true,
+                halign: Gtk.Align.CENTER
+            });
+            grid.insert_after(this.get_content_area(), null);
+
+            let label1 = new Gtk.Label({
+                halign: Gtk.Align.START,
+                label: keyLabel
+            });
+            grid.attach(label1, 0, 0, 1, 1);
+            this._entry1 = new Gtk.Entry({
+                text: key
+            });
+            grid.attach(this._entry1, 1, 0, 1, 1);
+
+            let label2 = new Gtk.Label({
+                halign: Gtk.Align.START,
+                label: valueLabel
+            });
+            grid.attach(label2, 0, 1, 1, 1);
+            this._entry2 = new Gtk.Entry({
+                text: value
+            });
+            grid.attach(this._entry2, 1, 1, 1, 1);
 
             this.add_button(_('Cancel'), Gtk.ResponseType.CANCEL);
             this.add_button(_('Ok'), Gtk.ResponseType.OK);
-            this.show_all();
         }
 
-        setLabel(label){
-            this._label.set_text(label);
+        getKey(){
+            return this._entry1.get_text();
         }
-        getEntry(){
-            return this._entry.get_text();
+        setKey(key){
+            this._entry1.set_text(key);
+        }
+        getValue(){
+            return this._entry2.get_text();
+        }
+        setValue(value){
+            this._entry2.set_text(value);
         }
     }
 );
